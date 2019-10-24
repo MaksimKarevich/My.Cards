75a5ac10932cf0d9d9462157476960a0d4067b59
Maksim Karevich <maksim.karevich@gmail.com>
2019-10-24 16:45:47 +0300

    added login tests

---
+++ e2e_tests/loginTests.js
@@ -0,0 +1,42 @@
+module.exports = {
+  '@tags': ['all', 'login', 'positive'],
+  before: function(browser) {
+    console.log('Setting up... browser', typeof browser);
+  },
+
+  after: function(browser) {
+    console.log('Closing down... browser', typeof browser);
+    browser.end();
+  },
+
+  'Login Test Positive': function(browser) {
+    const input = {
+      username: '2773a5c09a-edca8c@inbox.mailtrap.io',
+      password: 'Qwe123'
+    };
+    
+    const url = 'http://localhost:3000/';
+    const elements = {
+      email: "input[name='email']",
+      password: "input[name='password']",
+      buttonLogin: 'button[type=\'submit\']',
+      buttonLogout: '//span[contains(text(),\'Sign out\')]',
+    };
+
+    browser
+      .url(url)
+      .waitForElementVisible(elements.email, 5000, 'Website is loaded')
+      .assert.urlContains('auth/login', 'You are on the Login page')
+      .assert.attributeContains(elements.buttonLogin, 'disabled', 'true',
+      'Button is disabled')
+      .setValue(elements.email, input.username)
+      .setValue(elements.password, input.password)
+      .click(elements.buttonLogin)
+      .useXpath()
+      .waitForElementVisible(elements.buttonLogout, 5000, 'Logout button is visible')
+      .assert.urlContains('/dashboard', 'You are Logged In')
+      .click(elements.buttonLogout)
+      .useCss()
+      .waitForElementVisible(elements.email, 5000, 'You are logged out');
+  },
+};
---
+++ e2e_tests/loginTestsNegative.js
@@ -0,0 +1,69 @@
+//.setValue(SELECTOR, ['', [browser.Keys.CONTROL, 'a']])
+//.keys('\ue003')
+// These elements used because .clearValue is not worked properly
+
+module.exports = {
+  '@tags': ['all', 'login', 'negative'],
+
+  before: function(browser) {
+    console.log('Setting up... browser', typeof browser);
+  },
+
+  after: function(browser) {
+    console.log('Closing down... browser', typeof browser);
+    browser.end();
+  },
+
+  'Login Tests Negative': function(browser) {
+    const url = 'http://localhost:3000/';
+    const elements = {
+      email: "input[name='email']",
+      password: "input[name='password']",
+      buttonLogin: 'button[type=\'submit\']',
+      buttonLogout: '//span[contains(text(),\'Sign out\')]',
+      iconX: '#root > main > h6 > button > span.MuiIconButton-label > svg',
+      error: '#root > main > h6 > div',
+    };
+
+    browser
+      //Navigate to Login page
+      .url(url)
+      .waitForElementVisible(elements.email, 5000, 'Website is loaded')
+      .assert.urlContains('auth/login', 'You are on the Login page')
+      .assert.attributeContains(elements.buttonLogin, 'disabled', 'true',
+      'Button is disabled')
+      //Send empty form
+      .setValue(elements.email, ' ')
+      .setValue(elements.password, ' ')
+      .assert.attributeContains(elements.email, 'aria-invalid', 'true',
+      'Email is required')
+      .assert.attributeContains(elements.password, 'aria-invalid', 'true',
+      'Password is required')
+      .setValue(elements.email, ['', [browser.Keys.CONTROL, "a"]])
+      .keys('\ue003')
+      .setValue(elements.password, ['', [browser.Keys.CONTROL, "a"]])
+      .keys('\ue003')
+      //Fill both field with invalid data
+      .setValue(elements.email, 'email.domain.com')
+      .setValue(elements.password, 'q')
+      .assert.attributeContains(elements.email, 'aria-invalid', 'true',
+      'Email is not valid')
+      .setValue(elements.email, ['', [browser.Keys.CONTROL, "a"]])
+      .keys('\ue003')
+      .setValue(elements.password, ['', [browser.Keys.CONTROL, "a"]])
+      .keys('\ue003')
+      //Login with correct email and incorrect password
+      .setValue(elements.email, 'email@domain.com')
+      .setValue(elements.password, 'q')
+      .click(elements.buttonLogin)
+      .waitForElementVisible(elements.error, 5000, 'Invalid credentials')
+      .click(elements.iconX)
+      .setValue(elements.password, ['', [browser.Keys.CONTROL, "a"]])
+      .keys('\ue003')
+      //Valid email and empty password
+      .assert.attributeContains(elements.password, 'aria-invalid', 'true',
+      'Password is required')
+      //Case Sensitive email
+      //Add later
+  },
+};
